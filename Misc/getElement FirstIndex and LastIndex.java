9f0b71224aaae0b40b154a9f14dadab4635f33f3
Tejas Mawande <tejasmawande96@gmail.com>
2024-06-04 22:58:22 +0530

    Update getElement FirstIndex and LastIndex.java
    Cleanup

--- Misc/getElement FirstIndex and LastIndex.java
+++ Misc/getElement FirstIndex and LastIndex.java
@@ -6,33 +6,31 @@ public class JavaMain {
         int arr[] = {1,2,2,2,2,3,4,7,8,8,1};
 
             int maxValue = Integer.MIN_VALUE;
+            int minValue = Integer.MAX_VALUE;
+            int range = 0;
 
-               for(int a : arr){
-                  if(a>maxValue)
-                    maxValue = a;
-               }
-             int[] firstIndex = new int[maxValue+1];
-             int[] lastIndex = new int[maxValue+1];
+            int firstIndex[] = new int[100];
+            int[] lastIndex = new int[100];
 
-             for(int i=0; i <maxValue; i++){
-                 firstIndex[i] =  -1;
-                 System.out.println("firstIndex "+firstIndex[i]);
-             }
-
-            for(int i = 0; i <arr.length; i++) {
-                int element = arr[i];
-
-                    if(firstIndex[element] == -1){
-                       firstIndex[element] = i;
-                       System.out.println("firstIndex "+firstIndex[element]);
-                     }
-                lastIndex[element]= i;
+            for(int i =0 ; i <arr.length;i++ ){
+                firstIndex[i] = -1;
             }
 
-            for(int i =0; i <arr.length; i++){
-                if(firstIndex[i] != -1) {
-                    System.out.println("element +  firstIndex + LastIndex " + " " +i+ " "  + firstIndex[i] + " " + lastIndex[i]);
+            for(int i =0 ; i< arr.length; i++){
+                int element = arr[i];
+                if(firstIndex[element] == -1){
+                    firstIndex[element] = i;
+                }
+                lastIndex[element] = i;
+            }
+
+            for(int i = 0 ; i <arr.length; i++){
+                int element = arr[i];
+                if(firstIndex[element] != -1){
+                    System.out.println("element + firstIndex + lastIndex"+element+ " "+firstIndex[element]+ " "+lastIndex[element]);
+                    firstIndex[element] = -1;
                 }
             }
        }
-   }
+
+}
